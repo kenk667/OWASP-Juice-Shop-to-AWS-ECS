c8e5fdbea9af98692cb77cba38e9a059795b3573
ken l <ken@kindo.ai>
2024-12-30 16:17:48 -0500

    refatored region name with var.aws_region

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "subnet1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = var.aws_region
   
   tags = {
     Name = "subnet1"
@@ -30,7 +30,7 @@ resource "aws_subnet" "subnet1" {
 resource "aws_subnet" "subnet2" {
   vpc_id = aws_vpc.main.id 
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = var.aws_region
   
   tags = {
     Name = "subnet2" 
